c2eca847b473f6b286f00d6587be2d4e8228e048
胡标 <biao.hu@baishan.com>
2022-12-25 19:14:25 +0800

    feat: 补充binding包

--- gin.go
+++ gin.go
@@ -15,6 +15,7 @@ const (
 	contextPkg         = protogen.GoImportPath("context")
 	ginPkg             = protogen.GoImportPath("github.com/gin-gonic/gin")
 	metadataPkg        = protogen.GoImportPath("google.golang.org/grpc/metadata")
+	bindingPkg         = protogen.GoImportPath("github.com/gin-gonic/gin/binding")
 	deprecationComment = "// Deprecated: Do not use."
 )
 
@@ -34,7 +35,7 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) *protogen.Generated
 	g.P("// This is a compile-time assertion to ensure that this generated file")
 	g.P("// is compatible with the mohuishou/protoc-gen-go-gin package it is being compiled against.")
 	g.P("// ", contextPkg.Ident(""), metadataPkg.Ident(""))
-	g.P("// ", ginPkg.Ident(""))
+	g.P("// ", ginPkg.Ident(""), bindingPkg.Ident(""))
 	g.P()
 
 	for _, service := range file.Services {
